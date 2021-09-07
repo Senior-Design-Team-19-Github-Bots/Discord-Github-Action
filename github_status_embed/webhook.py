ac271e228081b7491ae397328537db6a938d73d3
Zheng <jialinzheng96@gamil.com>
2021-09-07 15:31:40 -0400

    Updata issue function

--- github_status_embed/webhook.py
+++ github_status_embed/webhook.py
@@ -11,6 +11,7 @@ log = logging.getLogger(__name__)
 
 EMBED_DESCRIPTION = "GitHub Actions run [{run_id}]({run_url}) {status_verb}."
 PULL_REQUEST_URL = "https://github.com/{repository}/pull/{number}"
+ISSUE_URL = "https://github.com/{repository}/issues/{number}"
 WEBHOOK_USERNAME = "GitHub Actions"
 WEBHOOK_AVATAR_URL = (
     "https://raw.githubusercontent.com/"
@@ -69,6 +70,54 @@ def get_payload_pull_request(
     )
     return webhook_payload
 
+def get_payload_issue(
+        workflow: types.Workflow, issue: types.Issue
+) -> types.WebhookPayload:
+    """Create a WebhookPayload with information about a Pull Request."""
+    # Calculate the character budget for the Source Branch field
+    author = issue.issue_author_login
+    workflow_number = f"{workflow.name} #{workflow.number}"
+    status = issue.issue_status
+
+    fields = [
+        types.EmbedField(
+            name="Issue Author",
+            value=f"[{author}]({issue.author_url})",
+            inline=True,
+        ),
+        types.EmbedField(
+            name="Workflow Run",
+            value=f"[{workflow_number}]({workflow.url})",
+            inline=True,
+        ),
+        types.EmbedField(
+            name="Issue Status ",
+            value=f"[{status}]({workflow.url})",
+            inline=True,
+        ),
+    ]
+
+    embed = types.Embed(
+        title=(
+            f"[{workflow.repository}] Checks {workflow.status.adjective} on issue: "
+            f"#{issue.number} {issue.title}"
+        ),
+        description=EMBED_DESCRIPTION.format(
+            run_id=workflow.id, run_url=workflow.url, status_verb=workflow.status.verb,
+        ),
+        url=ISSUE_URL.format(
+            repository=workflow.repository, number=issue.number
+        ),
+        color=workflow.status.color,
+        fields=fields,
+    )
+
+    webhook_payload = types.WebhookPayload(
+        username=WEBHOOK_USERNAME,
+        avatar_url=WEBHOOK_AVATAR_URL,
+        embeds=[embed]
+    )
+    return webhook_payload
 
 def get_payload(workflow: types.Workflow) -> types.WebhookPayload:
     """Create a WebhookPayload with information about a generic Workflow run."""
@@ -113,15 +162,19 @@ def send_webhook(
         workflow: types.Workflow,
         webhook: types.Webhook,
         pull_request: typing.Optional[types.PullRequest],
+        issue: typing.Optional[types.Issue],
         dry_run: bool = False,
 ) -> bool:
     """Send an embed to specified webhook."""
-    if pull_request is None:
-        log.debug("Creating payload for non-Pull Request event")
-        payload = get_payload(workflow)
-    else:
+    if issue is not None:
+        log.debug("Creating payload for Pull Request Check")
+        payload = get_payload_issue(workflow, pull_request)
+    elif pull_request is not None:
         log.debug("Creating payload for Pull Request Check")
         payload = get_payload_pull_request(workflow, pull_request)
+    else:
+        log.debug("Creating payload for non-Pull Request event")
+        payload = get_payload(workflow)
 
     log.debug("Generated payload:\n%s", json.dumps(payload, indent=4))
 
